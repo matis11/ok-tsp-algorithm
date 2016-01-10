78f36859a3488e0c81de444b22ac87fb36663abc
matis11 <geekbajt@gmail.com>
2016-01-10 22:50:53 +0100

    Added awesome help info

--- ok-tsp-algorithm.py
+++ ok-tsp-algorithm.py
@@ -4,7 +4,31 @@ import argparse
 
 from random import randint
 
-argparser = argparse.ArgumentParser()
+argparser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+argparser.description = " _____                   _ _ _\n" \
+                        "|_   _|                 | | (_)            \n" \
+                        "  | |_ __ __ ___   _____| | |_ _ __   __ _ \n" \
+                        "  | | '__/ _` \ \ / / _ \ | | | '_ \ / _` |\n" \
+                        "  | | | | (_| |\ V /  __/ | | | | | | (_| |\n" \
+                        "  \_/_|  \__,_| \_/ \___|_|_|_|_| |_|\__, |\n" \
+                        " _____       _                        __/ |\n" \
+                        "/  ___|     | |                      |___/     \n" \
+                        "\ `--.  __ _| | ___  ___ _ __ ___   __ _ _ __  \n" \
+                        " `--. \/ _` | |/ _ \/ __| '_ ` _ \ / _` | '_ \ \n" \
+                        "/\__/ / (_| | |  __/\__ \ | | | | | (_| | | | |\n" \
+                        "\____/ \__,_|_|\___||___/_| |_| |_|\__,_|_| |_|\n" \
+                        "______          _     _                \n" \
+                        "| ___ \        | |   | |               \n" \
+                        "| |_/ / __ ___ | |__ | | ___ _ __ ___  \n" \
+                        "|  __/ '__/ _ \| '_ \| |/ _ \ '_ ` _ \ \n" \
+                        "| |  | | | (_) | |_) | |  __/ | | | | |\n" \
+                        "\_|  |_|  \___/|_.__/|_|\___|_| |_| |_|\n" \
+                        "\n" \
+                        "Usage:\n" \
+                        "   By default, this script will calculate TSP using default algorithm on 5 nodes." \
+                        " You can change it using: \n" \
+                        "   1. '-n' to specify quantity of nodes,\n" \
+                        "   2. '-d' to specify distances between nodes (NOTE: remember about proper formatting)"
 argparser.add_argument("-d", "--distances", action="store", type=int, nargs='+',
                        help="Distances between nodes")
 argparser.add_argument("-n", "--nodes", action="store", type=int,
@@ -25,10 +49,6 @@ def parse_distances():
             value = distances.pop(0)
             graph[i][j] = value
             graph[j][i] = value
-            print(i, j, graph[i][j])
-            print("-")
-    print("__________")
-    print(graph)
 
 
 def generate_random_graph():
